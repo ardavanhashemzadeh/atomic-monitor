fddf7914c8419d40917dcf4febaebbaa9fc5deda
Adam Bro <atomicbrod101@gmail.com>
2017-07-13 20:41:57 -0500

    Used different API to retrieve uptime

--- agent/bin/boot_time.py
+++ agent/bin/boot_time.py
@@ -1,31 +1,8 @@
-import psutil
-import time
-
-
-# convert to # hours, minutes, seconds
-def sec_to_time(raw_sec):
-    s = int(time.time()) - psutil.boot_time()
-
-    minute = 60
-    hour = minute * 60
-    day = hour * 24
-
-    d = s / day
-    s -= d * day
-    h = s / hour
-    s -= h * hour
-    m = s / minute
-    s -= m * minute
-
-    return d, h, m, s
+from uptime import boottime
 
 
 class BootTime:
     # get system boot time
     def get_boot_time(self):
-        status = psutil.boot_time()
-
-        print(status)
-
-        # day(s), hour(s), minute(s), second(s)
-        return sec_to_time(status)
+        # datetime of boot
+        return boottime().strftime('%x %X')
