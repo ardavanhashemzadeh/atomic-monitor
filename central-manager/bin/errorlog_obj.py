3e1b17bd7a635b8d517a8a1b71a3174b483e48e1
Adam Bro <atomicbrod101@gmail.com>
2017-07-30 10:47:06 -0500

    Renamed _servername to _name

--- central-manager/bin/errorlog_obj.py
+++ central-manager/bin/errorlog_obj.py
@@ -1,19 +1,6 @@
 class ErrorLog:
-    def __init__(self, _servername, _timestamp, _type, _msg):
-        self.servername = _servername
+    def __init__(self, _name, _timestamp, _type, _msg):
+        self.name = _name
         self.timestamp = _timestamp
         self.typ = _type
         self.msg = _msg
-
-    def get_servername(self):
-        return self.servername
-
-    def get_timestamp(self):
-        # TODO
-        return self.timestamp
-
-    def get_type(self):
-        return self.typ
-
-    def get_msg(self):
-        return self.msg
